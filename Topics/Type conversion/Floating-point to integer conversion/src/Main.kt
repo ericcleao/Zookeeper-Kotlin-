1856b8ecefd78648fc74a69546eae4000da20658
Eric C. Leão <ericcleao@gmail.com>
2025-04-21 01:47:02 +0100

    chore(type-conversion): fetch topic

---
+++ Topics/Type conversion/Floating-point to integer conversion/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // read a floating-point number
+    val floatNumber = readln().toDouble()
+
+    // TODO: Convert the floating-point number to an integer
+
+    // TODO: Print the converted integer
+
+}
---
+++ Topics/Type conversion/Sum of Longs/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    // write your code here
+    val firstInput = readln()
+    val secondInput = readln()
+    println(firstInput.toLong()+secondInput.toLong())
+}
