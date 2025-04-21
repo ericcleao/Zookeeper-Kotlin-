f134606f6d56549b28001571a412c6dd7d26cb69
Eric C. Leão <ericcleao@gmail.com>
2025-04-21 21:10:33 +0100

    feat(mutable-map): solve Help the robot

--- Topics/Mutable Map/Help the robot/src/Main.kt
+++ Topics/Mutable Map/Help the robot/src/Main.kt
@@ -1,4 +1,7 @@
-fun helpingTheRobot(purchases: Map<String, Int>, addition: Map<String, Int>) : MutableMap<String, Int> {
-    //write your code here
-
+fun helpingTheRobot(purchases: Map<String, Int>, addition: Map<String, Int>): MutableMap<String, Int> {
+    return purchases.toMutableMap().apply {
+        for ((key, value) in addition) {
+            merge(key, value, Int::plus)
+        }
+    }
 }
