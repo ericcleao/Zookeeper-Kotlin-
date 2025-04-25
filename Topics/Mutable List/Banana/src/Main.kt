aab3834d557965fbc9333567a2f267b4083c7e63
Eric C. Leão <ericcleao@gmail.com>
2025-04-25 18:04:06 +0100

    feat(mutable-list): solve Banana! topic

--- Topics/Mutable List/Banana/src/Main.kt
+++ Topics/Mutable List/Banana/src/Main.kt
@@ -1,3 +1,5 @@
 fun solution(strings: MutableList<String>, str: String): MutableList<String> {
-    // put your code here
+    return strings
+        .map { if (it == str) "Banana" else it }
+        .toMutableList()
 }
