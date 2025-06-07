ab117e88d5b212f010b54c1a1808194096dd6934
Eric C. Leão <ericcleao@gmail.com>
2025-06-07 21:34:20 +0100

    feat(nested-and-inner-classes): solve Pumpkin and candle

--- Topics/Nested and inner classes/Pumpkin and candle/src/Main.kt
+++ Topics/Nested and inner classes/Pumpkin and candle/src/Main.kt
@@ -1,6 +1,13 @@
 class Pumpkin(val type: String, val isForHalloween: Boolean) {
 
-    // create function addCandle()
+    fun addCandle() {
+        if (isForHalloween) {
+            val candle = Candle()
+            candle.burning()
+        } else {
+            println("We don't need a candle.")
+        }
+    }
 
     inner class Candle {
         fun burning() {
