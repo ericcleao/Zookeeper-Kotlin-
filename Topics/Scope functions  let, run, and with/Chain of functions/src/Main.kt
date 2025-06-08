bee383424e070bdf64b0b45783936ce7e23e258f
Eric C. Leão <ericcleao@gmail.com>
2025-06-08 07:27:50 +0100

    chore(scope-functions-let-run-and-with): fetch Scope functions  let, run, and with"

---
+++ Topics/Scope functions  let, run, and with/Chain of functions/src/Main.kt
@@ -0,0 +1,13 @@
+data class Musician(var name: String = "", var band: String = "", var role: String = "")
+
+fun main() {
+    val musician = Musician()
+
+    musician.apply {
+        name = readln()
+        band = readln()
+        role = readln()
+    }
+
+    // Write your code here
+}
