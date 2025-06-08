ea4ff7a83ef642c525e9fe9d7d3a85abdce29ad2
Eric C. Leão <ericcleao@gmail.com>
2025-06-08 07:52:11 +0100

    chore(scope-functions-let-run-and-with): fetch Organize your code

---
+++ Topics/Scope functions  let, run, and with/Organize your code/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val day = readln()
+    val month = readln()
+    val year = readln()
+
+    val data = "$month.$day.$year"
+
+    // Do not change the code below 
+    println(data)
+}
