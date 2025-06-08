b528e75650a1f638504c08bea1b87a073494fb74
Eric C. Leão <ericcleao@gmail.com>
2025-06-08 06:52:24 +0100

    chore(scope-functions-apply-and-also): fetch Text field parameters

---
+++ Topics/Scope functions  apply and also/Text field parameters/src/Main.kt
@@ -0,0 +1,11 @@
+data class TextField(
+    var text: String = "Hello!",
+    var textSize: Int = 12,
+    var fontFamily: String = "Roboto"
+)
+
+fun main() {
+    val textField = TextField()
+
+    textField // Write your code here
+}
